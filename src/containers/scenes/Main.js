a88efe921b9d0345225d927a744639b3de2443fd
szymonsitko <simon@linux.home>
2017-07-16 14:26:52 +0200

    Main page desing has been implemented

--- src/containers/scenes/Main.js
+++ src/containers/scenes/Main.js
@@ -24,16 +24,18 @@ class Main extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={{ fontFamily: 'Visitor', fontSize: 82, textAlign: 'center', marginTop: height * .05 }}>PickerBox</Text>
+        <Image source={require('../../../assets/drawable/background_box.png')} style={styles.backgroundImage} />
+        <View style={{ position: 'absolute', marginLeft: 12, alignItems: 'center'}}>
+          <Text style={{ color: 'white', fontFamily: 'Visitor', fontSize: 88, textAlign: 'center'}}>PickerBox</Text>
+        </View>
         <View style={{ flex: 1, position: 'absolute', right: 0, bottom: 0, padding: 4 }}>
-          <Button title="Play" onPress={() => Actions.settings()} style={styles.buttonText} />
-          <Button title="Records" onPress={this.showRecordsModal.bind(this)} style={styles.buttonText} />
-          <Button title="Info" onPress={this.showInfoModal.bind(this)} style={styles.buttonText} />
+          <Button title="Play" onPress={() => Actions.settings()} style={[styles.buttonText, { color: '#d5f6ffff'} ]} />
+          <Button title="Records" onPress={this.showRecordsModal.bind(this)} style={[styles.buttonText, { color: '#d5f6ffff'} ]} />
+          <Button title="Info" onPress={this.showInfoModal.bind(this)} style={[styles.buttonText, { color: '#d5f6ffff'} ]} />
         </View>
         <Popup showModal={this.showInfoModal.bind(this)} modalVisible={this.state.infoModalVisible}>
 
           <Header style={{ fontFamily: 'Visitor' }} title="Info" background="#06939B"/>
-          
 
         </Popup>
         <Popup showModal={this.showRecordsModal.bind(this)} modalVisible={this.state.recordsModalVisible}>
@@ -52,7 +54,14 @@ const styles = {
     fontSize: 58,
     padding: 2,
     textAlign: 'right',
-    fontFamily: 'Visitor'
+    fontFamily: 'Visitor',
+    color: 'white'
+  },
+  backgroundImage: {
+    flex: 1,
+    width: width,
+    height: height,
+
   }
 }
 
